06d0f3425e05241d3ec76ba72b2bb21e52f11957
Atchett <Atchett@users.noreply.github.com>
2020-05-08 11:36:03 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+//====================================================================
+const MyStaticComponent = () => <div>Event cleanup: completed</div>;
+//====================================================================
+const MyMouseComponent = ({ myHrStyleProps }) => {
+  const [myMouseObject, myBooleanFunction] = useState({
+    myClientX: "Waiting...",
+    myClientY: "Waiting...",
+  });
+  const mouseMoveFunction = (event) => {
+    myBooleanFunction({
+      myClientX: event.clientX,
+      myClientY: event.clientY,
+    });
+  };
+  //--------------------------------------------------------------------
+  useEffect(() => {
+    document.addEventListener("mousemove", mouseMoveFunction);
+    return () => {
+      document.removeEventListener("mousemove", mouseMoveFunction);
+    };
+  }, []);
+  //--------------------------------------------------------------------
+  return (
+    <div>
+      <div>Move your mouse around</div>
+      <hr style={myHrStyleProps} />
+      <div>ClientX: {myMouseObject.myClientX}</div>
+      <div>ClientY: {myMouseObject.myClientY}</div>
+    </div>
+  );
+};
+//====================================================================
+const MyApp = () => {
+  const [myBoolean, myBooleanFunction] = useState(true);
+  const myChangWebpageFunction = () => {
+    myBooleanFunction(!myBoolean);
+  };
+  //--------------------------------------------------------------------
+  const myContainerStyle = {
+    maxWidth: "300px",
+    height: "110px",
+    margin: "10px auto",
+    padding: "6px 0",
+    backgroundColor: "#ddd",
+    textAlign: "center",
+    border: "1px solid #000",
+    fontSize: "16px",
+    fontFamily: "Helvetica",
+  };
+  const myHrStyle = { margin: "5px 0" };
+  //--------------------------------------------------------------------
+  return (
+    <div style={myContainerStyle}>
+      <button type="button" onClick={myChangWebpageFunction}>
+        {myBoolean ? "Force event cleanup" : "Return to mouse events"}
+      </button>
+      <hr style={myHrStyle} />
+      <div>
+        {myBoolean ? (
+          <MyMouseComponent myHrStyleProps={myHrStyle} />
+        ) : (
+          <MyStaticComponent />
+        )}
+      </div>
+    </div>
+  );
+};
+ReactDOM.render(<MyApp />, document.getElementById("root"));
